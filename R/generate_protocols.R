97191a34ead579791edcac36ad8cdb9416579ea2
fidadoma <filip.dechterenko@gmail.com>
2022-03-29 11:29:16 +0200

    finish generating protocols

--- R/generate_protocols.R
+++ R/generate_protocols.R
@@ -4,8 +4,12 @@ set.seed(789)
 
 videos <- read_table("R/data/triplets_sortedByReference.txt", col_names = F)
 # wait till final version
-videos <- videos[1:1218,]
-videos <- videos %>% mutate(across(.fns = ~sprintf("%03d",.x)))
+stopifnot(nrow(videos) == 1218)
+
+# format names
+videos <- videos %>% 
+  mutate(across(.fns = ~sprintf("%03d",.x))) %>% 
+  mutate(video_triplet_id = 1:n())
 
 n_videos <- nrow(videos)
 file_names <- 1:n_videos
@@ -21,20 +25,27 @@ protocols <-
   slice(rep(1:n(), times = 3)) %>% 
   mutate(n_repeat = rep(1:n_repeats,each = n_videos))
 
-protocols %>% 
-  group_by(n_repeat) %>% 
-  count()
-
-
-
 protocols <- protocols %>% 
   group_by(n_repeat) %>% 
   sample_frac(1) %>% 
   ungroup() %>% 
   mutate(prot_id = rep(1:n_protocols, each = videos_per_subject))
 
+protocols %>% jsonlite::write_json("protocol.json")
+
+# some simple sanity checks
+
+# all 87
 protocols %>% 
   group_by(prot_id) %>% 
   count()
 
-protocols %>% jsonlite::write_json("xx.json")
+# all threes
+protocols %>% 
+  group_by(video_triplet_id) %>% 
+  count()
+
+# all 1218
+protocols %>% 
+  group_by(n_repeat) %>% 
+  count()

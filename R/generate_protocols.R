6b8fac622e029d802db7b6d599c44d43e5e76452
fidadoma <filip.dechterenko@gmail.com>
2022-04-26 17:55:48 +0200

    create two version of protocols

--- R/generate_protocols.R
+++ R/generate_protocols.R
@@ -16,20 +16,49 @@ file_names <- 1:n_videos
 
 videos_per_subject <- 87
 
-n_repeats <- 3
+n_repeats <- 2
 
 n_protocols <- n_repeats*(n_videos / videos_per_subject)
 
 protocols <- 
   videos %>% 
-  slice(rep(1:n(), times = 3)) %>% 
+  slice(rep(1:n(), times = 2)) %>% 
   mutate(n_repeat = rep(1:n_repeats,each = n_videos))
 
-protocols <- protocols %>% 
+protocols_A <- protocols %>% 
   group_by(n_repeat) %>% 
   sample_frac(1) %>% 
   ungroup() %>% 
-  mutate(prot_id = rep(1:n_protocols, each = videos_per_subject))
+  mutate(prot_id = rep(1:n_protocols, each = videos_per_subject)) %>% 
+  mutate(version = "A")
+
+protocols_B <- protocols_A %>% 
+  rename(tmp = X2) %>% 
+  rename(X2 = X3) %>% 
+  rename(X3 = tmp) %>% 
+  mutate(version = "B")
+protocols <- rbind(protocols_A,protocols_B)
+
+# test
+
+stimuli_list <- readxl::read_excel(here::here("R/data/pilot_wood_list.xlsx"))
+protocols2 <- protocols %>% 
+  mutate(name = sprintf("%s_%s_%s.mp4", X1, X2,X3))
+swapped_names <- stimuli_list %>% filter(type == "swapped") %>% pull(name)
+normal_names <- stimuli_list %>% filter(type != "swapped") %>% pull(name)
+
+normal_names_protocols <- protocols2 %>% 
+  filter(version == "A") %>% pull(name)
+swapped_names_protocols <- protocols2 %>% 
+  filter(version == "B") %>% pull(name)
+
+intersect(normal_names, normal_names_protocols)
+setdiff(normal_names, normal_names_protocols)
+setdiff(normal_names_protocols, normal_names)
+
+intersect(swapped_names, swapped_names_protocols)
+setdiff(swapped_names, swapped_names_protocols)
+setdiff(swapped_names_protocols, swapped_names)
 
 protocols %>% jsonlite::write_json("protocol.json")
 
@@ -37,15 +66,21 @@ protocols %>% jsonlite::write_json("protocol.json")
 
 # all 87
 protocols %>% 
-  group_by(prot_id) %>% 
+  group_by(prot_id,version) %>% 
   count()
 
-# all threes
+# all fours
 protocols %>% 
   group_by(video_triplet_id) %>% 
   count()
 
+# all twos
+protocols %>% 
+  group_by(video_triplet_id,version) %>% 
+  count()
+
+
 # all 1218
 protocols %>% 
-  group_by(n_repeat) %>% 
+  group_by(n_repeat,version) %>% 
   count()

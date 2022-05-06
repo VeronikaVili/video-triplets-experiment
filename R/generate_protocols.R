aba270d1cedc06544d728edd9ed8109114f3b742
fidadoma <filip.dechterenko@gmail.com>
2022-05-06 09:15:30 +0200

    revert back to Jiri's version

--- R/generate_protocols.R
+++ R/generate_protocols.R
@@ -7,8 +7,8 @@ videos <- read_table("R/data/triplets_sortedByReference.txt", col_names = F)
 stopifnot(nrow(videos) == 1218)
 
 # format names
-videos <- videos %>%
-  mutate(across(.fns = ~sprintf("%03d",.x))) %>%
+videos <- videos %>% 
+  mutate(across(.fns = ~sprintf("%03d",.x))) %>% 
   mutate(video_triplet_id = 1:n())
 
 n_videos <- nrow(videos)
@@ -20,36 +20,36 @@ n_repeats <- 2
 
 n_protocols <- n_repeats*(n_videos / videos_per_subject)
 
-protocols <-
-  videos %>%
-  slice(rep(1:n(), times = 2)) %>%
+protocols <- 
+  videos %>% 
+  slice(rep(1:n(), times = 2)) %>% 
   mutate(n_repeat = rep(1:n_repeats,each = n_videos))
 
-protocols_A <- protocols %>%
-  group_by(n_repeat) %>%
-  sample_frac(1) %>%
-  ungroup() %>%
-  mutate(grp_id = rep(1:n_protocols, each = videos_per_subject)) %>% # 1-28
+protocols_A <- protocols %>% 
+  group_by(n_repeat) %>% 
+  sample_frac(1) %>% 
+  ungroup() %>% 
+  mutate(prot_id = rep(1:n_protocols, each = videos_per_subject)) %>% 
   mutate(version = "A")
 
-protocols_B <- protocols_A %>%
-  rename(tmp = X2) %>%
-  rename(X2 = X3) %>%
-  rename(X3 = tmp) %>%
+protocols_B <- protocols_A %>% 
+  rename(tmp = X2) %>% 
+  rename(X2 = X3) %>% 
+  rename(X3 = tmp) %>% 
   mutate(version = "B")
 protocols <- rbind(protocols_A,protocols_B)
 
 # test
 
 stimuli_list <- readxl::read_excel(here::here("R/data/pilot_wood_list.xlsx"))
-protocols2 <- protocols %>%
-  mutate(name = sprintf("%s_%s_%s.mp4", X1, X2,X3))
+protocols2 <- protocols %>% 
+  mutate(name = sprintf("%s_%s_%s.mp4", X1, X2, X3))
 swapped_names <- stimuli_list %>% filter(type == "swapped") %>% pull(name)
 normal_names <- stimuli_list %>% filter(type != "swapped") %>% pull(name)
 
-normal_names_protocols <- protocols2 %>%
+normal_names_protocols <- protocols2 %>% 
   filter(version == "A") %>% pull(name)
-swapped_names_protocols <- protocols2 %>%
+swapped_names_protocols <- protocols2 %>% 
   filter(version == "B") %>% pull(name)
 
 intersect(normal_names, normal_names_protocols)
@@ -60,41 +60,40 @@ intersect(swapped_names, swapped_names_protocols)
 setdiff(swapped_names, swapped_names_protocols)
 setdiff(swapped_names_protocols, swapped_names)
 
-prot_ids <- protocols %>% 
-  select(grp_id,version) %>% 
-  distinct() %>% 
-  mutate(prot_id = 1:n())
-
-protocols <- protocols %>% 
-  left_join(prot_ids,by = c("grp_id", "version")) %>% 
-  select(prot_id,grp_id,version,n_repeat,X1,X2,X3,video_triplet_id) %>% 
-  group_by(prot_id) %>% 
-  sample_frac(1) %>% 
-  ungroup()
-
-protocols %>% jsonlite::write_json("protocol.json")
-
-protocols %>%
-  nest_by(prot_id) %>%
-  jsonlite::write_json("protocol_list.json")
+# export protocols for js
+p <- protocols %>% 
+  mutate(
+    name = sprintf("%s_%s_%s.mp4", X1, X2, X3),
+    pid = str_c(str_pad(prot_id, 3, side = "left", pad = "0"), version)
+  )
+p_list <- p %>% group_split(pid)
+p_names <- p_list %>% map_chr(~unique(.$pid))
+p_text <- p_list %>% 
+  map(~list(video = .$name)) %>% 
+  set_names(p_names) %>% 
+  jsonlite::toJSON()
+final_json <- str_c("const protocols = ", p_text, ";\n")
+readr::write_file(final_json, file = "protocols.js")
 
 # some simple sanity checks
+
 # all 87
-protocols %>%
-  group_by(prot_id,version) %>%
+protocols %>% 
+  group_by(prot_id,version) %>% 
   count()
 
 # all fours
-protocols %>%
-  group_by(video_triplet_id) %>%
+protocols %>% 
+  group_by(video_triplet_id) %>% 
   count()
 
 # all twos
-protocols %>%
-  group_by(video_triplet_id,version) %>%
+protocols %>% 
+  group_by(video_triplet_id,version) %>% 
   count()
 
+
 # all 1218
-protocols %>%
-  group_by(n_repeat,version) %>%
+protocols %>% 
+  group_by(n_repeat,version) %>% 
   count()

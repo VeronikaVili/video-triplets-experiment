4519a4c7f2b632f42a580fbfe22362add3846150
Filip Děchtěrenko <filip.dechterenko@gmail.com>
2022-10-11 09:53:49 +0200

    Filip's analysis of rating experiment and analysis of simulation experiment

--- R/test_different_results.R
+++ R/test_different_results.R
@@ -2,28 +2,61 @@ library(tidyverse)
 
 library(jsonlite)
 library(purrr)
+library(RcppCNPy)
 
 fs <- list.files(here::here("R/data/triplets/EXP1"),full.names = T, recursive = T) %>% 
   as_tibble()
 
-fs <- fs %>% filter(str_detect(value,".json")) %>% 
-  mutate(params = str_remove(value,"D:/Documents/git/video-triplets-experiment/R/data/triplets/EXP1/4variants_")) %>% 
-  separate(params,into = c("lrate",NA,"lambda",NA), sep = "_") %>% 
+fs_json <- fs %>% 
+  filter(str_detect(value,".json")) %>% 
+  filter(str_detect(value,"_ix")) %>% 
+  mutate(params = str_remove(value,"C:/git/video-triplets-experiment/R/data/triplets/EXP1/4variants_")) %>% 
+  mutate(params = str_remove(params,"/results.json")) %>% 
+  separate(params,into = c("lrate",NA,"lambda",NA,"ix"), sep = "_") %>% 
   mutate(lrate = str_replace(lrate,"lrate","0.") %>% as.numeric(),
          lambda = str_replace(lambda,"lmbda","0.") %>% as.numeric())
 
-df <- fs %>% 
+
+fs_npy <- fs %>% 
+  filter(str_detect(value,".npy")) %>% 
+  filter(str_detect(value,"_ix")) %>% 
+  mutate(params = str_remove(value,"C:/git/video-triplets-experiment/R/data/triplets/EXP1/4variants_")) %>% 
+  mutate(params = str_remove(params,"/weights_sorted.npy")) %>% 
+  separate(params,into = c("lrate",NA,"lambda",NA,"ix"), sep = "_") %>% 
+  mutate(lrate = str_replace(lrate,"lrate","0.") %>% as.numeric(),
+         lambda = str_replace(lambda,"lmbda","0.") %>% as.numeric())
+
+df <- fs_json %>% 
   rowwise() %>% 
   mutate(train_accuracy = jsonlite::read_json(value)$train_acc,
          val_accuracy = jsonlite::read_json(value)$val_acc,
          val_loss = jsonlite::read_json(value)$val_loss)
 
+library(reticulate)
+np <- import("numpy")
+
+df_dim <- fs_npy %>% 
+  rowwise() %>% 
+  mutate(ndim = ncol(np$load(value)))
+
+df <- df %>% 
+  left_join(df_dim %>% select(-value), by = c("lrate", "lambda", "ix"))
+
+
+
 df
 df %>% 
   filter(train_accuracy>0) %>% 
   ggplot(aes(x = lrate, y = val_accuracy)) + 
-  geom_point() + 
+  stat_summary(fun.data="mean_cl_boot") +
   facet_grid(~lambda)
+
+df %>% 
+  filter(train_accuracy>0) %>% 
+  ggplot(aes(x = lrate, y = ndim)) + 
+  stat_summary(fun.data="mean_cl_boot") +
+  facet_grid(~lambda)
+
 
 p <- df %>% 
   filter(train_accuracy>0) %>% 
@@ -45,3 +78,9 @@ df %>%
   ggplot(aes(x = lrate, y = lambda, size = val_accuracy,group = lrate)) + 
   geom_point()+
   geom_segment()
+
+
+
+# pca ---------------------------------------------------------------------
+
+read_table()
